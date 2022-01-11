cbb11a7cfa9853a8465433cc67188491a81dbe46
KazKB <66971386+KazKB@users.noreply.github.com>
2022-01-11 19:41:12 -0400

    But the necessary folders in

---
+++ JavaScript/utility.js
@@ -0,0 +1,15 @@
+<!-- 
+function popUpMenu() {
+  var menu = document.getElementById("pop-up-menu"); //Gets Navigation Bar
+  if (menu.style.display === "block") {
+    menu.style.display = "none"; //Sets It To Disappear
+  } 
+  else {
+    menu.style.display = "block"; //Sets It To Reappear
+  }
+}
+
+function traverse(source) {
+  window.location.href = source;
+}
+//-->
